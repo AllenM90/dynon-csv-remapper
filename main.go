9c3e32447c3b397553b227343fb05ec104af8beb
AllenM90 <32987268+AllenM90@users.noreply.github.com>
2026-05-25 13:07:41 -0500

    Add Fyne file dialogs, config load, and process stub to main.go

--- main.go
+++ main.go
@@ -1,17 +1,65 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"os"
+
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
 )
+
+type Config map[string]string
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Dynon CSV Remapper")
+
+	var inputPath, outputPath string
+	config := loadConfig()
+
+	inputBtn := widget.NewButton("Select Input Dynon CSV", func() {
+		dialog.ShowFileOpen(func(f fyne.URIReadCloser, err error) {
+			if err == nil && f != nil {
+				inputPath = f.URI().Path()
+				f.Close()
+			}
+		}, w)
+	})
+
+	outputBtn := widget.NewButton("Select Output Location", func() {
+		dialog.ShowFileSave(func(f fyne.URIWriteCloser, err error) {
+			if err == nil && f != nil {
+				outputPath = f.URI().Path()
+				f.Close()
+			}
+		}, w)
+	})
+
+	processBtn := widget.NewButton("Process", func() {
+		if inputPath == "" || outputPath == "" {
+			dialog.ShowInformation("Missing", "Select both input and output", w)
+			return
+		}
+		// TODO: read CSV, apply config map, write output
+		fmt.Println("Config loaded:", config)
+		dialog.ShowInformation("Done", "Processing complete (stub)", w)
+	})
+
 	w.SetContent(container.NewVBox(
-		widget.NewLabel("Select input CSV, output location, then Process"),
-		widget.NewButton("Process", func() {}),
+		widget.NewLabel("Dynon EMS → Savvy Aviation CSV Remapper"),
+		inputBtn,
+		outputBtn,
+		processBtn,
 	))
 	w.ShowAndRun()
+}
+
+func loadConfig() Config {
+	data, _ := os.ReadFile("config.json")
+	var c Config
+	json.Unmarshal(data, &c)
+	return c
 }
